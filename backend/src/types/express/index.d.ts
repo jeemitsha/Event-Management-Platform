fe55dbffc5e72d922beb9238e7ec1592dc384170
jeemitsha <51538320+jeemitsha@users.noreply.github.com>
2025-02-07 23:40:58 +0530

    Update index.d.ts

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,11 +1,12 @@
-import { Request } from "express";
-import { Document } from "mongoose";
+import { Types } from "mongoose";
 import { IUser } from "../../models/User";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: Document<unknown, any, IUser> & IUser & { _id: Types.ObjectId };
+      user?: IUser & { _id: Types.ObjectId };
+      body: any;
+      params: any;
     }
   }
 }
