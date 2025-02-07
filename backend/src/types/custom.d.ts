ff74b19be56d59c1444b4d5a1ca75be82fba4c97
jeemitsha <51538320+jeemitsha@users.noreply.github.com>
2025-02-07 23:45:55 +0530

    Update custom.d.ts

--- backend/src/types/custom.d.ts
+++ backend/src/types/custom.d.ts
@@ -1,7 +1,23 @@
-import { Request } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Types } from "mongoose";
 import { IUser } from "../models/User";
 
 export interface AuthRequest extends Request {
   user: IUser & { _id: Types.ObjectId };
 }
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser & { _id: Types.ObjectId };
+    }
+  }
+}
+
+export type AsyncRequestHandler = (
+  req: Request | AuthRequest,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+export type UploadedFile = Express.Multer.File;
